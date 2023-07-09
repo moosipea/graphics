3980b0454a992344199d6c5913e5ef2d5f87d9b0
moosipea <mattiasmcvolt@gmail.com>
2023-07-09 18:48:40 +0300

    glfw working

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 #include "spritedata.h"
 
+#define WIDTH 800
+#define HEIGHT 600
+#define TITLE "graphics"
+
 int main() {
-    printf("Hello, world!\n");
+    if (!glfwInit())
+        return 1;
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
+    if (!window)
+        return 1;
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
